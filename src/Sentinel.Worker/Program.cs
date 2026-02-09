fe4629dda1e2de8f8320cec6de26ef037a36a119
Brian Mwenda <brianmwenda255@gmail.com>
2026-02-09 09:34:40 +0300

    feat: publish worker logs to api

--- src/Sentinel.Worker/Program.cs
+++ src/Sentinel.Worker/Program.cs
@@ -1,6 +1,9 @@
 using Sentinel.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.Configure<ApiOptions>(
+    builder.Configuration.GetSection(ApiOptions.SectionName));
+builder.Services.AddHttpClient();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
